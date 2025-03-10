dd203269e175d8febac682f3ecc1c7bdfaf9be31
LM Commande <contact@lmcommande.fr>
2025-03-10 13:41:48 +0100

    Fix Button component imports and file naming

--- src/components/ui/ConfirmationModal.tsx
+++ src/components/ui/ConfirmationModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from './Button';
+import { Button } from '@/components/ui/button';
 import { ReactNode } from 'react';
 
 export interface ConfirmationModalProps {
